463984c22fdeed531480f337efd00640c0c211ba
Luana Paes <luanapaes.dev@gmail.com>
2025-09-26 12:58:13 -0300

    feat: add rota de login e recipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Recipes } from './pages/recipes/recipes';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login,
+        title: 'Faça Login'
+    }, 
+    {
+        path: 'recipes',
+        component: Recipes,
+        title: "Página Inicial",
+        canActivate: [AuthGuard]
+    }
+];
