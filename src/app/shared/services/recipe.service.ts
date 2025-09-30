c98e09076fd0668ed9781778d073f38c8d7ccf35
Luana Paes <luanapaes.dev@gmail.com>
2025-09-30 08:00:34 -0300

    feat: add função `create` para criar novas receitas ao RecipeService

---
+++ src/app/shared/services/recipe.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CreateRecipe } from '../interfaces/create-recipe';
+import { AuthService } from './auth.service';
+import { Usuario } from '../interfaces/usuario.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  readonly apiUrl = 'http://localhost:3000/recipes';
+  httpClient = inject(HttpClient);
+  authService = inject(AuthService)
+
+  create(recipe: CreateRecipe) {
+    return this.httpClient.post<CreateRecipe>(this.apiUrl, recipe);
+  }
+}
