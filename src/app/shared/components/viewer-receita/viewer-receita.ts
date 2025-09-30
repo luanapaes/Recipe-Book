262267ca4c0a04970ca17fe17d603a4d18da8d9e
Luana Paes <luanapaes.dev@gmail.com>
2025-09-30 12:38:22 -0300

    feat: implementa função openEditDialog que abre o dialog e ao fichar emite o objeto atualizado

--- src/app/shared/components/viewer-receita/viewer-receita.ts
+++ src/app/shared/components/viewer-receita/viewer-receita.ts
@@ -1,9 +1,11 @@
-import { Component, inject, Inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Inject, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MAT_DIALOG_DATA, MatDialogContent, MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { Recipe } from '../../interfaces/recipe';
 import { RecipeService } from '../../services/recipe.service';
 import { MsgSnackBarService } from '../../services/msg-snackbar.service';
+import { EditRecipeDialog } from '../edit-recipe-dialog/edit-recipe-dialog';
+import { NotificationService } from '../../services/notification.service';
 @Component({
   selector: 'app-viewer-receita',
   imports: [MatDialogActions, MatDialogClose, MatButtonModule, MatDialogContent
@@ -19,7 +21,9 @@ export class ViewerReceita implements OnInit{
 
   receita!: Recipe;
   recipeService = inject(RecipeService)
-  msgSnackBar = inject(MsgSnackBarService)
+  msgSnackBar = inject(MsgSnackBarService);
+  notificationService = inject(NotificationService)
+  readonly dialog = inject(MatDialog);
 
   ngOnInit(): void {
     this.receita = this.data.receita
@@ -30,6 +34,23 @@ export class ViewerReceita implements OnInit{
       next: () => {
         this.dialogRef.close(id)
         this.msgSnackBar.openSnackBar("Receita deletada com sucesso!")
+      }
+    })
+  }
+
+
+  openDialogEdit(){
+    const dialogRef = this.dialog.open(EditRecipeDialog, {
+      data: this.data.receita
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (updatedRecipe) => {
+        this.notificationService.sendNotification(updatedRecipe)
+        return this.receita = {
+          ...this.receita,
+          ...updatedRecipe
+        }
       }
     })
   }
