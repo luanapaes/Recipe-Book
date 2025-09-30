09d87dfcf50dd963e70bb849be5597a4fda3ef7c
Luana Paes <luanapaes.dev@gmail.com>
2025-09-30 10:53:51 -0300

    feat: implementa função delete que utiliza o service para deletar um areceita por id

--- src/app/shared/components/viewer-receita/viewer-receita.ts
+++ src/app/shared/components/viewer-receita/viewer-receita.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MAT_DIALOG_DATA, MatDialogContent } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe.service';
+import { MsgSnackBarService } from '../../services/msg-snackbar.service';
 @Component({
   selector: 'app-viewer-receita',
   imports: [MatDialogActions, MatDialogClose, MatButtonModule, MatDialogContent
@@ -10,10 +12,25 @@ import { Recipe } from '../../interfaces/recipe';
   styleUrl: './viewer-receita.css'
 })
 export class ViewerReceita implements OnInit{
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { receita: Recipe }) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { receita: Recipe },
+    private dialogRef: MatDialogRef<ViewerReceita>
+  ) { }
+
   receita!: Recipe;
+  recipeService = inject(RecipeService)
+  msgSnackBar = inject(MsgSnackBarService)
 
   ngOnInit(): void {
     this.receita = this.data.receita
+  }
+
+  delete(id: number){
+    return this.recipeService.delete(id).subscribe({
+      next: () => {
+        this.dialogRef.close(id)
+        this.msgSnackBar.openSnackBar("Receita deletada com sucesso!")
+      }
+    })
   }
 }
