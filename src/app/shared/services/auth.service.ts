02d3c8fec49e5cbec890d196e86bf223c1ba6e45
Luana Paes <luanapaes.dev@gmail.com>
2025-09-30 08:00:03 -0300

    feat: add função `login` e `getToken` ao AuthService

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  urlApi = 'http://localhost:3000/auth';
+  httpClient = inject(HttpClient);
 
   isBrowser(): boolean {
     return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
@@ -11,5 +15,13 @@ export class AuthService {
 
   logado() {
     return this.isBrowser() && localStorage.getItem('token') ? true : false;
+  }
+
+  login(email: string, password: string) {
+    return this.httpClient.post(`${this.urlApi}/signin`, { email, password });
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
   }
 }
